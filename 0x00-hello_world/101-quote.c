2c25098467689b8f0282b66600ce0d43a1b60bd6
dagmawi <dagmawiparis2016@gmail.com>
2022-06-03 07:12:27 -0700

    a script that prints eactly

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - pints and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * followed by new line
+ * Return 1 if (Success)
+ */
+int main (void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpan, 2015-10-19\n", 59);
+return(1);
+}

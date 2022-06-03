50965f0c34c6995890cf301ca39e0a10c9f17793
dagmawi <dagmawiparis2016@gmail.com>
2022-06-03 06:28:50 -0700

    size data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of char: 1 byte(s)\n")
+printf("Size of int: 4 byte(s)\n") 
+printf("Size of long int: 4 byte(s)\n") 
+printf("Size of long long int : 8 byte(s)\n") 
+printf("Size of float: 4 byte(s)\n") 
+return(0);
+}

d5b20cc1bc950a54ceb4d24336592319457607a9
dagmawi <dagmawiparis2016@gmail.com>
2022-06-03 07:16:21 -0700

    a script that prints eactly unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - pints and that piece of art is useful\" - Dora Korpar, 2015-10-19
- * followed by new line
- * Return 1 if (Success)
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpan, 2015-10-19\n", 59);
-return(1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }

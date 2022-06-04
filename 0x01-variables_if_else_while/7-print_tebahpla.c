ab2fdd8033d51467ebb3ff0fc85cf0f0de8b5937
dagmawi <dagmawiparis2016@gmail.com>
2022-06-04 07:38:11 -0700

    printing a to z reverced

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: printing the alphabet in reverce
+ * Return: 0
+ */
+int main(void)
+{
+	char x = 'z';
+
+	while (x = 'a')
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);

b82f1cae171aa68f7d11dc1cc47cc434dbb33b0a
dagmawi <dagmawiparis2016@gmail.com>
2022-06-04 05:51:32 -0700

    listing the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main -main block
+ * Description: Get a list from a to z
+ * Return: 0
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+
+	putchar('\n');
+	return(0);
+}

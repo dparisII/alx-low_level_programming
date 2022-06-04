49c26ed332d312353dbff88dbac6964766aab1a2
dagmawi <dagmawiparis2016@gmail.com>
2022-06-04 05:55:39 -0700

    listing the alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
- * main -main block
+ * main - main block
  * Description: Get a list from a to z
  * Return: 0
  */
@@ -16,5 +16,5 @@ int main(void)
 	}
 
 	putchar('\n');
-	return(0);
+	return (0);
 }

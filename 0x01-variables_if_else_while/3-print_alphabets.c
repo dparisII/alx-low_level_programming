3466d69a8b358165fd45b3dffaf48dca2dbccf7b
dagmawi <dagmawiparis2016@gmail.com>
2022-06-04 06:25:06 -0700

    listing a to z small then in capital

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 /**
- * main - main 
- * Descrption: thisis about listing the alphabet in small leter and then capital letter
+ * main - main
+ * Descrption: thisis about listing the alphabet a - z & A - Z
  * Return: 0
  */
 
@@ -12,16 +12,16 @@ int main(void)
 int x = 'a';
 int z = 'A';
 
-while (x <= 'z')
-	{
-		putchar(x);
-		x++;
-	}	
-while (z <= 'Z')
-	{
-		putchar(z);
-		z++;
-	}
+	while (x <= 'z')
+		{
+			putchar(x);
+			x++;
+		}	
+	while (z <= 'Z')
+		{
+			putchar(z);
+			z++;
+		}
 putchar('\n');
 return (0);
 }

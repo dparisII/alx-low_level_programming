e79c7e8763359f895033f096ef9842b279540eaf
dagmawi <dagmawiparis2016@gmail.com>
2022-06-05 13:13:34 -0700

    program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,31 +3,37 @@
 /**
  * main - main block
  * Description: Is  a program that prints all possible different combinations of two digits
+ * different combinations of two digits
+ * You are not allowed to use any variable of type char
+ * Print only the smallest combination of two digits
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
  * Return: 0
  */
 int main(void)
 {
-int c;
-int d = 0;
-while (d < 10)
-{
-c = 0;
-while (c < 10)
-{
-if (d != c && d < c)
-{
-putchar('0' + d);
-putchar('0' + c);
-if (c + d != 17)
-{
-putchar(',');
-putchar(' ');
-}
-}
-c++;
-}
-d++;
-}
-putchar('\n');
-return (0);
+	int c;
+	int d = 0;
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+				
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			c++;
+		}
+		d++;
+	}
+		putchar('\n');
+		return (0);
 }

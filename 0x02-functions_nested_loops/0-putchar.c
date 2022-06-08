6071420f8c210e3a49fd9b30e8b43e3199e6cb1d
dagmawi <dagmawiparis2016@gmail.com>
2022-06-08 11:03:16 -0700

    main.h function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,13 @@
  */
 int main(void)
 {
-	char word[9] = "_putchar";
-		int i;
+char word[9] = "_putchar";
+int i;
 
-	for (i = 0; i < 9; i++)
-		_putchar(word[i]);
-		_putchar('\n');
+for (i = 0; i < 9; i++)
+{
+	_putchar(word[i]);
+}
+	_putchar('\n');
 return (0);
 }

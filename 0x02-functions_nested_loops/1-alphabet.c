43c627c4b0b68d7a7276c12d49aba33835c68cb7
dagmawi <dagmawiparis2016@gmail.com>
2022-06-08 11:55:27 -0700

    listing the small letter alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 #include "main.h"
 /**
  * main - check the code
- * Description: prints the alphabet, in lowercase
+ * Description - prints the alphabet, in lowercase
  * Return: Always 0
  */
 void print_alphabet(void)

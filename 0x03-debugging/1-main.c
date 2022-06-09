db1ac57c738a812fb67d5b68ede577f0db0c1a83
dagmawi <dagmawiparis2016@gmail.com>
2022-06-09 11:04:19 -0700

    1-main.c file that does Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -8,16 +7,15 @@
 int main(void)
 {
 	int i;
-	
-	printf("infinite loop incoming :(\n");
 
+	printf("Infinite loop incoming :(\n");
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i);
-	}
-	
+	/* while (i < 10)*/
+	/* {*/
+	/*	putchar(i);*/
+	/* }*/
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);

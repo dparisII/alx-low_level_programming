f50fdaa7e51b625c488050c3d5fe9abb346cf044
dagmawi <dagmawiparis2016@gmail.com>
2022-06-09 11:49:16 -0700

    this is the largest no file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,16 +14,12 @@ int largest_number(int a, int b, int c)
 	
 
 	if (a >= b && a >= c)
-	
 	{
 		largest = a;
 	}
-	
 	else if (b >= a && b >= c)
 	{
-	
 		largest = b;
-	
 	}
 	else if (c >= a && c >= b)
 	{

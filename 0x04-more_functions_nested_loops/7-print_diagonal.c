64a4b6983680b0992fe4cba9adcd12cadd0d0133
dagmawi <dagmawiparis2016@gmail.com>
2022-06-10 02:47:44 -0700

    weitting dagonaly

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,13 +14,11 @@ void print_diagonal(int n)
 		for (; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
-			{
 				_putchar(' ');
-			}
-		_putchar(92);
-		_putchar('\n');			
+			_putchar(92);
+			_putchar('\n');			
 		}
 	}
 	else
-	_putchar('\n');
+		_putchar('\n');
 }

f06105205ccdca7d54de8baff748afc050c90165
dagmawi <dagmawiparis2016@gmail.com>
2022-06-09 22:53:11 -0700

    this is if the letter is upper it says upper and lower if its lowwer

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description:
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", C, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _islower(c));
+	return (0);
+}

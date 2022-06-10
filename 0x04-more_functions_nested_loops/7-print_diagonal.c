2954196c260e06adeefd34db60bd34388722da1c
dagmawi <dagmawiparis2016@gmail.com>
2022-06-10 02:35:21 -0700

    writing diagonaly

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * printing_diagonal - a function that draws a diagonal line on terminal.
+ * print_diagonal - a function that draws a diagonal line on the terminal.
  * @n: An input integer
  * Return: Always 0
  */
 void print_diagonal(int n)
 {
-	int i = 0;
-	int j;
+	int i = 0, j;
 
 	if (n > 0)
 	{
@@ -16,10 +15,10 @@ void print_diagonal(int n)
 		{
 			for (j = 0; j < i; j++)
 				_putchar(' ');
-		_putchar(92);
-		_putchar('\n');
-		}
+				_putchar(92);
+				_putchar('\n');
+													}
 	}
 	else
-		_putchar('\n');
+	_putchar('\n');
 }

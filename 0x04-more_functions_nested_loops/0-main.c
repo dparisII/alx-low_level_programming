de6dfc362e7cd1c2de62d50cb39143f2d6769e6e
dagmawi <dagmawiparis2016@gmail.com>
2022-06-09 23:08:27 -0700

    the main file for 0-isupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -10,8 +10,8 @@ int main(void)
 	char c;
 
 	c = 'A';
-	printf("%c: %d\n", C, _isupper(c));
+	printf("%c: %d\n", c, \_isupper(c));
 	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, \_isupper(c));
 	return (0);
 }

0c65e05ceb54300f1a722eeb09f76ce26849f167
dagmawi <dagmawiparis2016@gmail.com>
2022-06-10 06:06:09 -0700

    printing an integer using nested loop

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
+void print_integer(int m);
+
 /**
- * main
- * Return: void
+ * print_number - a function that prints an integer.
+ * @n: An input integer
+ * Return: Nothing
  */
 void print_number(int n)
 {
@@ -11,17 +14,23 @@ void print_number(int n)
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar_integer(n * -1);
+		print_integer(n * -1);
 	}
 	else
 		print_integer(n);
 }
+
+/**
+ * print_integer - A function to priting n
+ * @m: an input unsigned integer
+ * Return: Nothing
+ */
 void print_integer(int m)
 {
 	int i = 1000000000;
 
 	for (; i >= 1; i /= 10)
-		if (m /1 != 0)
+		if (m / i != 0)
 		{
 			_putchar((m / i) % 10 + '0');
 		}

29b8cf280301b869b86357106ebf1309b84c89db
dagmawi <dagmawiparis2016@gmail.com>
2022-06-10 00:53:34 -0700

    printing a set of number 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * printing 0 to 14 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j;
+
+	for (; i <= 10 i++)
+	{
+		j = 0;
+		for (; j < 15; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}

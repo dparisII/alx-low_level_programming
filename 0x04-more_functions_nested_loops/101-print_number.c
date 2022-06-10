b64a2d0c4a2deea5ddd5f3ee62309661c3898464
dagmawi <dagmawiparis2016@gmail.com>
2022-06-10 07:32:31 -0700

    printing an integer using nested loop

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,19 +14,19 @@ void print_number(int n)
 		_putchar(45);
 		m = n * -1;
         }
-	else
-        {
-	    m = n;
-	}
-	        d = m;
-		count = 1;
-	while (d > 9)
+  	else
 	{
+		m = n;
+  	}
+		d = m;
+		count = 1;
+    	while (d > 9)
+  	{
 		d /= 10;
 		count *= 10;
-        }
-  	for (; count >= 1; count /= 10)
+	}
+	for (; count >= 1; count /= 10)
         {
-    		_putchar(((m / count) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
   	}
 }

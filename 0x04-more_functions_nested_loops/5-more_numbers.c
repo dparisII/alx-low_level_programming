ee3c83bc62dbc7c1219243570b86a89a96f806d7
dagmawi <dagmawiparis2016@gmail.com>
2022-06-10 01:29:00 -0700

    listing the numbers 1 to 14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * printing 0 to 14 10 times
- * Return: void
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14,
+ * followed by a new line.
+ * Return: Always 0
  */
 void more_numbers(void)
 {
-	int i = 0;
-	int j;
+	int i, j = 0;
 
-	for (; i <= 10; i++)
+	for (; j < 10; j++)
 	{
-		j = 0;
-		for (; j < 15; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(j);
+			if (i > 9)
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }

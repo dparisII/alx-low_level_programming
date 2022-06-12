1ded739201056ec761f80e57223213e9553c2061
dagmawi <dagmawiparis2016@gmail.com>
2022-06-11 23:25:38 -0700

    the main function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int _abs - this function checks if it is  an absolute value
+ * int: the main type
+ * Return: 0
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+		return (r);
+	else
+		return (r * -r);
+}

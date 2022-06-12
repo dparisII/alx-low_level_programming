8c8513b69f276d2d207db69a9cf01312f35402f8
dagmawi <dagmawiparis2016@gmail.com>
2022-06-11 23:16:37 -0700

    prints a sign depending on the number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		value = 1;
-		_putchar("+");
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
-		vlaue = 0;
-		_putchar("0");
+		value = 0;
+		_putchar('0');
 	}
 	else
 	{
 		value = -1;
-		_putchar("-");
+		_putchar('-');
 	}
 	return (value);
 }

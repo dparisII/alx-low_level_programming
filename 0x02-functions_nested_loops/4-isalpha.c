7f0ef33015ee04b4bd37e62c85cce1ad5f7bc0ae
dagmawi <dagmawiparis2016@gmail.com>
2022-06-11 23:00:12 -0700

    this checks if the letter is an alphabet or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ * _isalpha - this checks if it leter is alphabet or not
+ * @c: is the input character
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	char i, j;
+	int isletter = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (j = 'A'; j <= 'Z'; j++)
+		{
+			if (c == i || c == j)
+				isletter = 1
+		}
+	}
+	return (isletter);
+}
+

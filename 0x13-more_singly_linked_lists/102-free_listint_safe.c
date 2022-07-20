1b7903daa22c0746309ab7082eff6d2279cbef35
dagmawi <dagmawiparis2016@gmail.com>
2022-07-20 01:35:59 -0700

    a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -40,11 +40,14 @@ size_t free_listint_safe(listint_t **h)
 	{
 		new = malloc(sizeof(listp_t));
 		if (new == NULL)
-		exit(98);
+			exit(98);
+	
 		new->p = (void *)*h;
 		new->next = hptr;
 		hptr = new;
+	
 		add = hptr;
+	
 		while (add->next != NULL)
 		{
 			add = add->next;
@@ -56,6 +59,7 @@ size_t free_listint_safe(listint_t **h)
 				return (nnodes);
 			}
 		}
+
 		curr = *h;
 		*h = (*h)->next;
 		free(curr);

80c162efa0174141eaa7d0163fca34938d1c10ea
dagmawi <dagmawiparis2016@gmail.com>
2022-07-26 15:57:22 -0700

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,8 +44,8 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
+	
 	nchars = 1024;
-
 	while (nchars == 1024)
 	{
 		nchars = read(file_from, buf, 1024);
